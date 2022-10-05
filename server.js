e03dff0f4aa99d8c85b02e8a4c6fba6933782557
ckrum21 <108544020+ckrum21@users.noreply.github.com>
2022-10-05 11:27:59 -0400

    server.js setup

--- server.js
+++ server.js
@@ -5,3 +5,26 @@ const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use (express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '092100Dance',
+        database: 'employeeTracker_db'
+    },
+    console.log('Connected to the employeeTracker_db.')
+);
+
+//add content
+
+app.use((req, res) =>{
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
